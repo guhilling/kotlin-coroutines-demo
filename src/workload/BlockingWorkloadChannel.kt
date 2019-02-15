ae5b9d6b5461d6161e43d0f7f51529d32388d900
Gunnar Hilling <gunnar@hilling.de>
2019-02-15 14:52:36 +0100

    version 1.0

--- src/workload/BlockingWorkloadChannel.kt
+++ src/workload/BlockingWorkloadChannel.kt
@@ -6,23 +6,27 @@ import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeoutOrNull
 
 fun main(): Unit = runBlocking {
     val pool = newFixedThreadPoolContext(2, "worker pool")
     val timesAccumulator = Channel<Long>(0)
 
-    (1..4).forEach {
-        launch(pool) {
-            timesAccumulator.send(doWork("task-${it}"))
+    val sendjob = launch {
+        (1..4).forEach {
+            launch(pool) {
+                timesAccumulator.send(doWork("task-${it}"))
+            }
         }
     }
 
     var total = 0L
-    withTimeoutOrNull(1000) {
+    launch {
         timesAccumulator.consumeEach {
             total += it
             log("current total time ${total} ms")
         }
-    }?:Unit
+    }
+    sendjob.join()
+    timesAccumulator.close()
+    Unit
 }
